2d5579b0ee01f0c4a38e55d222f5c4ed93e561a5
dzhitanska <43849176+dzhitanska@users.noreply.github.com>
2018-12-15 17:29:34 +0200

    Add files via upload

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -35,6 +35,19 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+      $("#login_modal").hide();
+        $("#login").click(function(){
+        $("#login_modal").show();
+
+    });
+
+    // hide
+
+        $("#login_close_button").click(function(){
+        $("#login_modal").hide();
+    });
+    });
 
                 
 
