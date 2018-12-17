c9d20fdc1f821ae4c94a1a02406c173d1ea562ee
dzhitanska <43849176+dzhitanska@users.noreply.github.com>
2018-12-17 15:15:36 +0200

    Add files via upload

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -34,20 +34,20 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1300);
     });
 });
-
 $(document).ready(function(){
-      $("#login_modal").hide();
-        $("#login").click(function(){
-        $("#login_modal").show();
-
+    $("#toto").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1300);
     });
+});
 
-    // hide
 
-        $("#login_close_button").click(function(){
-        $("#login_modal").hide();
-    });
-    });
 
                 
 
